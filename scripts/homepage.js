134e026cfcf1b6e411a02a2640ca45a3003ef379
LAPTOP-7NO6ANAA\navee <rakeshsenapathi@gmail.com>
2021-10-17 22:05:53 +0530

    CSS Changes and animation for eclipse shape

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -37,4 +37,34 @@ $(document).ready(function () {
         resourcesOwlCarousel.next();
     });
 
+    $("#img-container--1").hover(function(){
+        $("#oval-shape-content--title").text("Image");
+        $("#oval-shape-content").css({"left" : "12%", "top" : "41%", "transition" : "all ease-in .2s"});
+        $("#oval-shape-content--description").text("Neque, a orci ut tincidunt. Eros, facilisi vulputate eu donec pharetra. Tempus sit lectus pharetra aliquet. Tortor aenean duis leo in.");
+    });
+
+    $("#img-container--2").hover(function(){
+        $("#oval-shape-content--title").text("Text");
+        $("#oval-shape-content").css({"left" : "32%", "top" : "41%", "transition" : "all ease-in .2s"});
+        $("#oval-shape-content--description").text("Neque, a orci ut tincidunt. Eros, facilisi vulputate eu donec pharetra. Tempus sit lectus pharetra aliquet. Tortor aenean duis leo in.");
+    });
+
+    $("#img-container--3").hover(function(){
+        $("#oval-shape-content--title").text("Video");
+        $("#oval-shape-content").css({"left" : "50%", "top" : "41%", "transition" : "all ease-in .2s"});
+        $("#oval-shape-content--description").text("Neque, a orci ut tincidunt. Eros, facilisi vulputate eu donec pharetra. Tempus sit lectus pharetra aliquet. Tortor aenean duis leo in.");
+    });
+
+    $("#img-container--4").hover(function(){
+        $("#oval-shape-content--title").text("3D Sensor Fusion");
+        $("#oval-shape-content").css({"left" : "69%", "top" : "41%", "transition" : "all ease-in .2s"});
+        $("#oval-shape-content--description").text("Neque, a orci ut tincidunt. Eros, facilisi vulputate eu donec pharetra. Tempus sit lectus pharetra aliquet. Tortor aenean duis leo in.");
+    });
+
+    $("#img-container--5").hover(function(){
+        $("#oval-shape-content--title").text("Mapping");
+        $("#oval-shape-content").css({"left" : "80%", "top" : "41%", "transition" : "all ease-in .2s"});
+        $("#oval-shape-content--description").text("Neque, a orci ut tincidunt. Eros, facilisi vulputate eu donec pharetra. Tempus sit lectus pharetra aliquet. Tortor aenean duis leo in.");
+    });
+
 });
