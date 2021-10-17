33ef79f23bee8fbb1e51b778ece25f9b60be09a9
Venkata Deekshith Reddy, Chilakala <cv00571220@techmahindra.com>
2021-10-18 03:36:30 +0530

    solutions page enhancements + image updates

--- scripts/solution_1.js
+++ scripts/solution_1.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
     $('header nav ul li .company-name').removeClass('color-white');
     $('header nav').addClass('color-black');
+
+    // Accordian Click Functionality for image change
+    $('#entity-annotation').click(() => {
+        if ($("#use-case-hero-img").attr('src') !== './../images/solutions_page/text_annotation/usecases/Entity Annotation.png') {
+            $("#use-case-hero-img").attr('src', './../images/solutions_page/text_annotation/usecases/Entity Annotation.png');
+        }
+    });
+    $('#text-classification').click(() => {
+        if ($("#use-case-hero-img").attr('src') !== './../images/solutions_page/text_annotation/usecases/Text Classification.png') {
+            $("#use-case-hero-img").attr('src', './../images/solutions_page/text_annotation/usecases/Text Classification.png');
+        }
+    });
+    $('#sentiment-analysis').click(() => {
+        if ($("#use-case-hero-img").attr('src') !== './../images/solutions_page/text_annotation/usecases/Sentiment Analysis.png') {
+            $("#use-case-hero-img").attr('src', './../images/solutions_page/text_annotation/usecases/Sentiment Analysis.png');
+        }
+    });
+    $('#linguistic-annotation').click(() => {
+        if ($("#use-case-hero-img").attr('src') !== './../images/solutions_page/text_annotation/usecases/Linguistic Annotation.png') {
+            $("#use-case-hero-img").attr('src', './../images/solutions_page/text_annotation/usecases/Linguistic Annotation.png');
+        }
+    });
+
+
 });
